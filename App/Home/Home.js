6ebeea2673f433823d1956d3fba5562ab5a90a69
aopina <angel.omar@alsea.com.mx>
2016-11-29 10:50:27 -0600

    Add files via upload

---
+++ App/Home/Home.js
@@ -0,0 +1,37 @@
+﻿/// <reference path="../App.js" />
+// global app
+var ord;
+
+(function () {
+    'use strict';
+
+    // The initialize function must be run each time a new page is loaded
+    Office.initialize = function (reason) {
+        $(document).ready(function () {
+            app.initialize();
+
+            $('#get-data-from-selection').click(getDataFromSelection);
+        });
+    };
+
+	function obtener_orden(orden){
+		ord = orden;
+		
+	}
+	function regresar_orden(){
+		return ord;
+	}
+
+    // Reads data from current document selection and displays a notification
+    function getDataFromSelection() {
+        Office.context.document.getSelectedDataAsync(Office.CoercionType.Text,
+			function (result) {
+			    if (result.status === Office.AsyncResultStatus.Succeeded) {
+			        app.showNotification('The selected text is:', '"' + result.value + '"');
+			    } else {
+			        app.showNotification('Error:', result.error.message);
+			    }
+			}
+		);
+    }
+})();
